bf2d4ca038201d04965bf7be7bd974939f897528
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-09-28 14:00:47 -0600

    Set up initial file structure with components

---
+++ src/components/App/helper.js
@@ -0,0 +1,5 @@
+import kinderData from '../../data/kindergartners_in_full_day_program';
+
+export default class DistrictRepository {
+
+}
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import DistrictRepository from './helper';
 
 class App extends Component {
+
+  
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>Headcount 2.0</div>
     );
   }
 }
