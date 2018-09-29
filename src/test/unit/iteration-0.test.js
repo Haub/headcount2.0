ea67aff755d896be1363588de674756fd767b39d
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-09-28 20:54:08 -0600

    Pass all tests for iteration 0

--- src/test/unit/iteration-0.test.js
+++ src/test/unit/iteration-0.test.js
@@ -1,17 +1,14 @@
-import DistrictRepository from '../../helper.js';
+import DistrictRepository from '../../components/App/helper.js';
 import kinderData from '../../data/kindergartners_in_full_day_program.js';
 
 describe('DistrictRepository iteration 0', () =>  {
   const district = new DistrictRepository(kinderData);
 
   test('district has data in an object', () => {
-    // remember that an array is also just an object.
     expect(typeof district.stats).toBe('object');
   });
 
   test('data coming in has no duplicates', () => {
-    // uncomment out the tests that best fits your model
-    // expect(district.stats.length).toBe(181);
     expect(Object.keys(district.stats).length).toBe(181);
   });
 
