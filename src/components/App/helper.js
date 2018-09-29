643fb24c248ff0900ddd4255906ab0258f5b489a
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-09-28 20:53:40 -0600

    Create object of district data in correct format

--- src/components/App/helper.js
+++ src/components/App/helper.js
@@ -1,5 +1,21 @@
 import kinderData from '../../data/kindergartners_in_full_day_program';
 
 export default class DistrictRepository {
+  
+  constructor(data) {
+    this.stats = this.removeDuplicates(kinderData)
+  }
 
+  removeDuplicates = (kinderData) => {
+    return kinderData.reduce((kinderObj, district) => {
+      let schoolDistrict = district.Location;
+      let year = district.TimeFrame
+      let roundedData = Math.round(district.Data * 1000)/1000
+      if(!kinderObj[schoolDistrict]) {
+        kinderObj[schoolDistrict] = {location: schoolDistrict, stats: {}}
+      } 
+      kinderObj[schoolDistrict].stats[year] = roundedData
+      return kinderObj
+    }, {})
+  }
 }
