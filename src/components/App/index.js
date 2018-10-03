11b61c587d3e04d9ad93e302020244874d102d95
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-10-03 14:20:51 -0600

    Add search functionality and cards are filtering correctly based on input field

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,32 +3,34 @@ import './App.css';
 import DistrictRepository from './helper';
 import kinderData from '../../data/kindergartners_in_full_day_program';
 import { CardContainer } from '../CardContainer';
+import Search from '../Search';
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       district: new DistrictRepository(kinderData),
       filteredDistricts: []
-    }
+    };
   }
   
   componentDidMount = () => {
-    this.filterDistricts()
+    this.setState({filteredDistricts: this.state.district.stats});
   }
 
   filterDistricts = (districtInput) => {
     const filteredDistricts = this.state.district.findAllMatches(districtInput);
-    this.setState({ filteredDistricts })
+    this.setState({ filteredDistricts });
   }
 
   render() {
     return (
       <div>
         <header>
-          <h1>Headcount 2.0</h1>
-          <CardContainer filteredDistricts={this.state.filteredDistricts} />
+          <h1 className='headcount-title'>Headcount 2.0</h1>
+          <Search filterDistricts={this.filterDistricts} />
         </header>
+        <CardContainer filteredDistricts={this.state.filteredDistricts} />
       </div>
     );
   }
