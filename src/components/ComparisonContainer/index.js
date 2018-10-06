60595038c9e5d824ef2c8da8e9400d18a4158bbc
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-10-06 17:48:00 -0600

    Add ComparisonContainer with compared cards

---
+++ src/components/ComparisonContainer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const ComparisonContainer = ({comparisonObject}) => {
+  // console.log(comparisonObject)
+
+  return(
+    <div>
+      <h2></h2>
+
+
+      <h2></h2>
+    </div>
+  )
+}
