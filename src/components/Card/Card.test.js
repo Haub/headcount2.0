13d841f641a8a6d1afd22d3105b510cba0e978a3
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-10-06 17:48:51 -0600

    Add card tests for classNames

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -17,12 +17,12 @@ describe ('CARD', () => {
   });
 
   it('should have class of below if stat value is below 0.5', () => {
-    wrapper=shallow(<Card location={'Academy 20'} mockStats= {{ 2004: 0.3, 2005: 0.6 }} />)
-    expect(wrapper.find('li').is('.below')).toHaveLength(1);
+    wrapper=shallow(<Card location={'Academy 20'} mockStats= {{ 2004: 0.3, 2005: 0.6 }} key={1} />)
+    expect(wrapper.find('li').is('.below')).toEqual(true);
   })
 
   it('should have class of above if stat value is above 0.5', () => {
-    wrapper=shallow(<Card location={'Academy 20'} mockStats= {{ 2004: 0.3, 2005: 0.6 }} />)
+    wrapper=shallow(<Card location={'Academy 20'} mockStats= {{ 2004: 0.3, 2005: 0.6 }} key={1} />)
     expect(wrapper.find('li').is('.above')).toHaveLength(1);
   })
 
