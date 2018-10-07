cbee5873de97dd13105c82c5872d220f136a9a4a
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-10-06 20:13:21 -0600

    Add selectLocationsfunction

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,6 +3,7 @@ import './App.css';
 import DistrictRepository from './helper';
 import kinderData from '../../data/kindergartners_in_full_day_program';
 import { CardContainer } from '../CardContainer';
+import { ComparisonContainer } from '../ComparisonContainer';
 import Search from '../Search';
 
 class App extends Component {
@@ -10,18 +11,28 @@ class App extends Component {
     super();
     this.state = {
       district: new DistrictRepository(kinderData),
-      filteredDistricts: []
+      filteredDistricts: [],
+      selectedDistricts: [],
+      comparisonObject: {}
     };
   }
   
   componentDidMount = () => {
-    // this.setState({ filteredDistricts: this.state.district.stats });
     this.filterDistricts(this.state.district.state);
   }
 
   filterDistricts = (districtInput) => {
     const filteredDistricts = this.state.district.findAllMatches(districtInput);
     this.setState({ filteredDistricts });
+  }
+
+  selectDistrict = (location) => {
+    const newDistrict = this.state.district.findByName(location);
+    if(this.state.selectedDistricts < 3 && !this.state.selectedDistricts.includes(newDistrict)) {
+      const selectedDistricts = [...this.state.selectedDistricts, newDistrict];
+      this.setState({selectedDistricts}, () => this.compareDistricts(this.state.selectedDistricts))
+    }
+
   }
 
   render() {
@@ -31,7 +42,12 @@ class App extends Component {
           <h1 className='headcount-title'>Headcount 2.0</h1>
           <Search filterDistricts={this.filterDistricts} />
         </header>
-        <CardContainer filteredDistricts={this.state.filteredDistricts} />
+        <ComparisonContainer comparisonObject={this.comparisonObject} /> 
+
+        <CardContainer 
+          filteredDistricts={this.state.filteredDistricts} 
+          selectDistrict={this.selectDistrict}
+        />
       </div>
     );
   }
