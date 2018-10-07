9a184a67480923e61099ab10a977a4179d881a7b
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-10-06 21:21:40 -0600

    Add comparisonContainer with correct comparison average data rendering properly

--- src/components/ComparisonContainer/index.js
+++ src/components/ComparisonContainer/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 export const ComparisonContainer = ({comparisonObject}) => {
-  // console.log(comparisonObject)
+  const locationOne = Object.keys(comparisonObject)[0];
+  const locationOneAverage = comparisonObject[locationOne];
+  const locationTwo = Object.keys(comparisonObject)[1];
+  const locationTwoAverage = comparisonObject[locationTwo];
+  const comparedAverage = comparisonObject.compared
 
   return(
     <div>
-      <h2></h2>
-
-
-      <h2></h2>
+      <h3>{locationOne}</h3>
+      <h3>{locationOneAverage}</h3>
+      <h2>{comparedAverage}</h2>
+      <h3>{locationTwo}</h3>
+      <h3>{locationTwoAverage}</h3>
     </div>
   )
 }
