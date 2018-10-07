9086ec1d8f92fb55718a54c39df43b5eba54a50e
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-10-06 21:44:42 -0600

    Add ComparisonCardsso that comparison capability is rendering correctly

---
+++ src/components/ComparisonCard/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types'; 
+
+export const ComparisonCard = ({location, stats, unselectDistrict}) => {
+ 
+   const displayStats = Object.keys(stats).map((stat, index) => {
+    return (
+      <li key={index} className={stats[stat] < 0.5 ? 'below': 'above'}>{stat}: {stats[stat]}</li>
+    )
+    
+  });
+  
+  return (
+    <div className='district-cards' onClick={()=>unselectDistrict(location)}>
+      <h2 className='location'>{location}</h2>
+      <ul className='stats'>{displayStats}</ul>
+    </div>
+  );
+};
+
+ComparisonCard.propTypes = {
+  location: PropTypes.string,
+  stats: PropTypes.object 
+};
